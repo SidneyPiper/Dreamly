b4c4438db7d7e4a69fd1d7cf42f611d6bb971fba
cainytheslave <wajo432@gmail.com>
2024-04-01 17:28:43 +0200

    chore: make Editor.vue typescript compliant

--- components/Editor.vue
+++ components/Editor.vue
@@ -1,44 +1,40 @@
 <template>
   <div class="flex flex-col grow w-full">
-    <div ref="inputRef" contenteditable="true" tabindex="0"
-      class="text-wrap first-line:text-3xl break-words grow px-4 focus:outline-none"
-      @keydown.enter.exact.prevent="lineBreak"><br>
+    <div ref="inputRef" class="text-wrap first-line:text-3xl break-words grow px-4 focus:outline-none"
+         contenteditable="true"
+         tabindex="0"
+         @keydown.enter.exact.prevent="lineBreak"><br>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-const inputRef = ref(null)
+<script lang="ts" setup>
+const inputRef = ref<HTMLDivElement>()
 
 onMounted(() => {
   focus()
 })
 
 const focus = () => {
-  inputRef.value.focus({ preventScroll: true })
+  inputRef.value!.focus({preventScroll: true})
 }
 
 const get = (): [string, string] => {
-  const newlineIndex = inputRef.value.innerText.indexOf('\n');
+  const newlineIndex = inputRef.value!.innerText.indexOf('\n');
 
-  // If there's no '\n', return the whole string as the first part, and an empty string as the second part
   if (newlineIndex === -1) {
-    return [inputRef.value.innerText, ""];
+    return [inputRef.value!.innerText, ""];
   }
 
-  const title = inputRef.value.innerText.substring(0, newlineIndex);
-  const content = inputRef.value.innerText.substring(newlineIndex + 1); // +1 to skip the '\n' character
+  const title = inputRef.value!.innerText.substring(0, newlineIndex);
+  const content = inputRef.value!.innerText.substring(newlineIndex + 1);
 
   return [title, content];
 }
 
-const getContent = () => {
-
-}
-
 const lineBreak = () => {
   const selection = window.getSelection();
-  const range = selection.getRangeAt(0);
+  const range = selection!.getRangeAt(0);
 
   const br = document.createElement('br');
   range.insertNode(br);
@@ -46,13 +42,13 @@ const lineBreak = () => {
   range.setStartAfter(br);
   range.setEndAfter(br);
 
-  selection.removeAllRanges();
-  selection.addRange(range);
+  selection!.removeAllRanges();
+  selection!.addRange(range);
 
 }
 
 defineExpose({
-  focus,
-  get
+  get,
+  focus
 })
 </script>
