77f640a64bdce9eb8e51b505de69444b8d14a6c9
cainytheslave <wajo432@gmail.com>
2024-04-01 18:31:52 +0200

    chore: make DataWithIcon.vue typescript compliant

--- components/DataWithIcon.vue
+++ components/DataWithIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-stretch bg-stone-800 rounded-md">
     <div class="flex items-center px-3 shrink-0">
-      <slot />
+      <slot/>
     </div>
 
     <input :value="props.value" class="grow px-0 block w-full bg-transparent border-transparent" disabled type="text">
@@ -9,5 +9,7 @@
 </template>
 
 <script lang="ts" setup>
-const props = defineProps(['value'])
+const props = defineProps<{
+  value: string
+}>()
 </script>
