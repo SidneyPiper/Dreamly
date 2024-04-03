a7bca3045d7f1412070a2a0e6165823c46ee3289
cainytheslave <wajo432@gmail.com>
2024-04-03 19:51:21 +0200

    feat: add favicon and all that boring stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,18 @@ export default defineNuxtConfig({
     },
     app: {
         head: {
-            viewport: 'width=device-width, initial-scale=1.0, interactive-widget=resizes-visual'
+            viewport: 'width:device-width, initial-scale:1.0, interactive-widget:resizes-visual',
+            link: [
+                {rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icon.png'},
+                {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png'},
+                {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png'},
+                {rel: 'manifest', href: '/site.webmanifest'},
+                {rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'},
+            ],
+            meta: [
+                {name: 'msapplication-TileColor', content: '#da532c'},
+                {name: 'theme-color', content: '#ffffff'}
+            ]
         }
     },
     googleFonts: {
