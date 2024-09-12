065fa9e0902bbd1fefa35846cf9acc67a462701c
SidneyPiper <sidlapip@gmail.com>
2024-09-12 14:37:16 +0200

    donate and privacy page

--- pages/donate.vue
+++ pages/donate.vue
@@ -8,9 +8,10 @@
     </div>
 
     <Fader class="grow text-stone-900" vertical>
-      <div class="h-full overflow-y-auto text-white px-4 pb-4 flex flex-col gap-6 space-y-1">
-        <LabeledText label="Buy us a Coffee">
-          <p>...</p>
+      <div
+          class="h-full overflow-y-auto text-white px-4 pb-4 flex flex-col gap-6 space-y-1 items-center justify-center">
+        <LabeledText label="Buy us a Coffee :)">
+          <a href="https://www.paypal.me/sidneypiper">paypal.me/sidneypiper</a>
         </LabeledText>
       </div>
     </Fader>
